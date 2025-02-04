ebf0e5ec000b49f85eb8956ed88f806a4fdf4d21
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-02-04 23:42:57 +0000

    search matching types

--- lib/ast.ml
+++ lib/ast.ml
@@ -6,3 +6,9 @@ type expr =
   | Type of string
   | Val of string * expr
   | Op of op * expr * expr
+
+let expr_types (expr: expr) : expr option =
+  match expr with
+  | Val (_, types) -> Some types
+  | Op _ -> None
+  | Type _ -> None
--- lib/search.ml
+++ lib/search.ml
@@ -1,9 +1,23 @@
 open Ast
 
-let rec contains (list_to_search : expr list) (expr_to_search : expr) : bool  =
+let rec contains (list_to_search : expr list) (expr_to_search : expr) : bool =
   match (list_to_search , expr_to_search) with
   | ([], _) -> false
   | (l, e) ->
     match compare (List.hd l) e with
     | 0 -> true
     | _ -> contains (List.tl l) e
+
+let match_types (list_to_search : expr list) (expr_to_search : expr) : expr list =
+  let expr_to_search = Ast.expr_types expr_to_search in
+  List.filter (
+    fun e ->
+    match (expr_to_search, Ast.expr_types e) with
+    | (Some a, Some b) ->
+      begin
+        match compare a b with
+        | 0 -> true
+        | _ -> false
+      end
+    | (_, _) -> false
+  ) list_to_search
